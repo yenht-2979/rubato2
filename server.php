dea31a61700f98305f3f6ca8600442d0b4bb6ea4
han thi yen <han.thi.yen@sun-asterisk.com>
2024-06-12 14:24:17 +0700

    D

--- server.php
+++ server.php
@@ -18,7 +18,9 @@ $uri = urldecode(
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
-    return false;
 }
 
 require_once __DIR__ . '/public/index.php';
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
